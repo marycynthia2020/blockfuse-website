c3585cd342b67b3f6e45336b5fc028f6dd3d11e1
Chinemerem Ugbaja <marycynthiaconfirm@gmail.com>
2025-02-18 15:11:13 +0100

    my commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        background: "url('/images/Background')",
+        web2: "url('/images/backdrop11.jpeg')",
+        web3: "url('/images/backdrop21.jpeg')",
+      },
+      fontFamily: {
+        space: ["Space Grotesk", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
